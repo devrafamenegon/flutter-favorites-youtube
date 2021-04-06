fe42eb6edf7f47192348a47a8a3e2f9d5e7d67c1
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-04-06 18:14:25 -0300

    Project configuration

---
+++ android/app/src/main/java/br/com/menegon/flutter_favorites_youtube/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.menegon.flutter_favorites_youtube;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/api.dart
@@ -0,0 +1,1 @@
+const API_KEY = "AIzaSyA9ntdY5OHNnBP2YtP8tVA3eRi02h9Capk";
