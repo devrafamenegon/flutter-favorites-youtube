45fb94538259109e8d44d772db934908c47697f7
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-04-06 23:48:13 -0300

    Creating the main layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,34 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset("assets/yt_logo_dark.png"),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text("0"),
+          ),
+          IconButton(
+              icon: Icon(Icons.star),
+              onPressed: (){
+
+              }
+          ),
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: (){
+
+              }
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
